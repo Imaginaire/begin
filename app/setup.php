116960d4b8116f4ffefad63e236f3f23f0fec790
theimaginaire <seb.dean@theimaginaire.com>
2019-02-11 09:37:16 +0000

    Added footer column widgets & implemented with template.

--- app/setup.php
+++ app/setup.php
@@ -90,8 +90,20 @@ add_action('widgets_init', function () {
         'id'            => 'sidebar-primary'
     ] + $config);
     register_sidebar([
-        'name'          => __('Footer', 'sage'),
-        'id'            => 'sidebar-footer'
+        'name'          => __('Footer Column 1', 'sage'),
+        'id'            => 'sidebar-footer-1'
+    ] + $config);
+    register_sidebar([
+        'name'          => __('Footer Column 2', 'sage'),
+        'id'            => 'sidebar-footer-2'
+    ] + $config);
+    register_sidebar([
+        'name'          => __('Footer Column 3', 'sage'),
+        'id'            => 'sidebar-footer-3'
+    ] + $config);
+    register_sidebar([
+        'name'          => __('Footer Column 4', 'sage'),
+        'id'            => 'sidebar-footer-4'
     ] + $config);
 });
 
--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,11 +1,39 @@
 <footer class="content-info float-left w-100">
   <div class="container">
-    @php dynamic_sidebar('sidebar-footer') @endphp
+    <div class="row">
+      <div class="col-md-3">
 
-    @option('copyright_notice')
+        @php dynamic_sidebar('sidebar-footer-1') @endphp
 
-    @option('company_info')
+      </div>
+      <div class="col-md-3">
 
-    @option('website_credit')
+        @php dynamic_sidebar('sidebar-footer-2') @endphp
+
+      </div>
+      <div class="col-md-3">
+
+        @php dynamic_sidebar('sidebar-footer-3') @endphp
+
+      </div>
+      <div class="col-md-3">
+
+        @php dynamic_sidebar('sidebar-footer-4') @endphp
+
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-md-12">
+
+        @option('copyright_notice')
+
+        @option('company_info')
+
+        @option('website_credit')
+
+      </div>
+    </div>
+
   </div>
 </footer>
