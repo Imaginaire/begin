d1a3b87d6b59b6ca868ec37dcc9afc52b8aa08e9
theimaginaire <seb.dean@theimaginaire.com>
2019-02-11 09:48:51 +0000

    Added hero block template.

---
+++ resources/views/blocks/hero.blade.php
@@ -0,0 +1,9 @@
+<div class="hero text-center p-5 my-4 float-left w-100" style="background-image:url(@field('hero_image'));">
+
+  <div class="{!! App::container() !!}">
+
+    @field('hero_content')
+
+  </div>
+
+</div>
